b25ec4c586390dae2b0ad29f4e9773b9d5bf3280
Sam Dolman <samueldolman@gmail.com>
2025-12-04 13:32:45 +0000

    separate out grid class

--- utils/grid.py
+++ utils/grid.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-#from collections import Counter
-import sys
-sys.path.append('utils/')
-import grid
-
-sample = False
-
-if sample:
-    filename = "inputs/day04_sample.txt"
-else:
-    filename = "inputs/day04.txt"
-
-def parse_input(filename):
-    with open(filename) as f:
-        puzzle_input = [l.strip() for l in f.readlines()]
-    return puzzle_input
-
 class Grid:
     def __init__(self, puzzle_input):
         if isinstance(puzzle_input[0], list):
@@ -93,30 +75,3 @@ class Grid:
                         self.assign(x, y, 'x')
                         ans += 1
         return ans
-        
-        
-
-        
-
-def solve(puzzle_input):
-    grid = Grid(puzzle_input)
-    
-    rolls = grid.find_removable_rolls()
-    ans1 = len(rolls)
-
-    grid.remove_rolls(rolls)
-
-    ans2 = ans1
-    while True:
-        new_removals = grid.find_and_remove_rolls()
-        ans2 += new_removals
-        if new_removals == 0:
-            break
-
-    return ans1, ans2
-
-puzzle_input = parse_input(filename)
-ans_1, ans_2 = solve(puzzle_input)
-
-print(ans_1)
-print(ans_2)
