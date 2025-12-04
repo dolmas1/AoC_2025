10fc7da064bed85652d3b93384583ba51558c661
Sam Dolman <samueldolman@gmail.com>
2025-12-04 19:10:09 +0000

    combine Day 1 pts 1 and 2

--- solns/day01.py
+++ solns/day01.py
@@ -11,48 +11,35 @@ def parse_input(filename):
         puzzle_input = [l.strip() for l in f.readlines()]
     return puzzle_input
 
-
-def solve_pt1(puzzle_input):
+def solve(puzzle_input):
     pos = 50
-    ans = 0
-    for l in puzzle_input:
-        if l[0] == 'L':
-            x = -1 * int(l[1:])
-        else:
-            x = int(l[1:])
-        pos += x
-        pos = pos % 100
-        if pos == 0:
-            ans += 1
-        #print(pos, ans)
-    return ans
-
-def solve_pt2(puzzle_input):
-    pos = 50
-    ans = 0
+    ans1 = 0
+    ans2 = 0
     for l in puzzle_input:
         x = int(l[1:])
+        #print('\n', l[0], x)
 
         div, x = divmod(x, 100)
         if div > 0:
-            ans += div
-        
-        if l[0] == 'R':
-            new_pos = (pos + x) % 100
-            if new_pos < pos:
-                ans += 1
-            pos = new_pos
-        else:
-            new_pos = (pos - x) % 100
-            if (new_pos > pos and pos > 0) | (new_pos == 0):
-                ans += 1
-            pos = new_pos
+            #print(div)
+            ans2 += div
+        if l[0] == 'L':
+            x = -x
 
-    return ans
+        new_pos = pos + x
+        newer_pos = new_pos % 100
+        if ((newer_pos != new_pos) & (pos > 0)):
+            ans2 += 1
+        elif newer_pos == 0:
+            ans2 += 1
+        if newer_pos == 0:
+            ans1 += 1
+        pos = newer_pos
+
+    return ans1, ans2
 
 puzzle_input = parse_input(filename)
-ans_1 = solve_pt1(puzzle_input)
-ans_2 = solve_pt2(puzzle_input)
+ans_1, ans_2 = solve(puzzle_input)
 
 print(ans_1)
 print(ans_2)
