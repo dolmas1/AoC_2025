601465c8f440d7093b5537fbec46f7d2bf5b1c22
Sam Dolman <samueldolman@gmail.com>
2025-12-04 12:26:50 +0000

    don't repeat day 4 pt 1

--- solns/day04.py
+++ solns/day04.py
@@ -12,7 +12,6 @@ def parse_input(filename):
         puzzle_input = [l.strip() for l in f.readlines()]
     return puzzle_input
 
-
 class Grid:
     def __init__(self, puzzle_input):
         if isinstance(puzzle_input[0], list):
@@ -21,6 +20,10 @@ class Grid:
             self.grid = [list(l) for l in puzzle_input]
         self.x_max = len(self.grid[0])
         self.y_max = len(self.grid)
+
+    def __str__(self):
+      "Method to show something human-readable when print() is called on a grid object"
+      return "\n".join(''.join([str(c) for c in row]) for row in self.grid)
 
     def lookup(self, x, y):
         if x >= self.x_max:
@@ -62,17 +65,21 @@ class Grid:
         neighbours = [self.lookup(x, y) for [x, y] in neighbour_coords]
         return [x for x in neighbours if x is not None]
 
-    def count_removable_rolls(self):
-        ans = 0
+    def find_removable_rolls(self):
+        rolls = []
         for x in range(self.x_max):
             for y in range(self.y_max):
                 if self.lookup(x, y) == '@':
                     neighbours = self.get_neighbours(x, y)
                     if Counter(neighbours)['@'] < 4:
-                        ans += 1
-        return ans
+                        rolls.append([x, y])
+        return rolls
 
-    def remove_rolls(self):
+    def remove_rolls(self, roll_locations):
+        for [x, y] in roll_locations:
+            self.assign(x, y, 'x')
+        
+    def find_and_remove_rolls(self):
         ans = 0
         for x in range(self.x_max):
             for y in range(self.y_max):
@@ -85,22 +92,27 @@ class Grid:
         
         
 
-def solve_pt1(grid):
-    return grid.count_removable_rolls()
+        
 
-def solve_pt2(grid):
-    ans = 0
+def solve(puzzle_input):
+    grid = Grid(puzzle_input)
+    
+    rolls = grid.find_removable_rolls()
+    ans1 = len(rolls)
+
+    grid.remove_rolls(rolls)
+
+    ans2 = ans1
     while True:
-        new_removals = grid.remove_rolls()
-        ans += new_removals
+        new_removals = grid.find_and_remove_rolls()
+        ans2 += new_removals
         if new_removals == 0:
             break
-    return ans
+
+    return ans1, ans2
 
 puzzle_input = parse_input(filename)
-grid = Grid(puzzle_input)
-ans_1 = solve_pt1(grid)
-ans_2 = solve_pt2(grid)
+ans_1, ans_2 = solve(puzzle_input)
 
 print(ans_1)
 print(ans_2)
